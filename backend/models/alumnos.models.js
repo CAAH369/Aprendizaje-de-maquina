e0d995e8e277a6678513ca97dd278166cb14a6e2
cristhianandrea <cristhianavilah@outlook.es>
2026-06-15 13:41:44 -0600

    Agrego todos los archivos

--- backend/models/alumnos.models.js
+++ backend/models/alumnos.models.js
@@ -1,10 +1,10 @@
-import{Schema, model} from "mongoose";
+import{Schema} from "mongoose";
 
 const Esqueleto_tabla = new Schema({
-     nombre:String,
-     edad:Number,
+     nombre:String
+     edad:Number
      materia:String
 })
 
-export const Tabla = model("Tabla de alumnos reprobados", Esqueleto_tabla);
+export const Tabla = model("Tabla de alumnos reprobados");
 
