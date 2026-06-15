bfac881679dd74d6e0447901cdcc9b887148f61c
cristhianandrea <cristhianavilah@outlook.es>
2026-06-15 13:11:04 -0600

    Agrego errores para la práctica

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
+import mongose from "mongoose";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import {test} from "./backend/controllers/alumnos.controllers.js"
 
 //conexion a la base de datos
-dotenv.config();
+dotenv.config;
 mongoose.connect(process.env.url_bd)
-.then(() => console.log("todo jala chido en la base de datos"))
-.catch((error) => console.log("no funciono este pedo"))
+.then(() console.log("todo jala chido en la base de datos"))
+.catch( => console.log("no funciono este pedo"))
 
 //creacion de un servidor local
 const app = express();
 app.use(cors());
 
-app.listen(4000, () => console.log("funciona el servidor"))
+app.listen( () => console.log("funciona el servidor"))
 
-test();
+test()
